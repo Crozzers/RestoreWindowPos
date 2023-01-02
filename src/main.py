6d60650b2e595dd17bf310071f5f83167c45fb7e
Crozzers <captaincrozzers@gmail.com>
2023-01-02 16:15:22 +0000

    Remove the `--install` flag.
    Creating the shorcut in `shell:startup` isn't necessary now that the installer exists

--- src/main.py
+++ src/main.py
@@ -12,14 +12,12 @@ import sys
 import threading
 import time
 
-import pythoncom
 import pywintypes
 import win32api
 import win32con
 import win32gui
 import win32gui_struct
 from infi.systray import SysTrayIcon
-from win32com.shell import shell
 
 GUID_DEVINTERFACE_DISPLAY_DEVICE = "{E6F07B5F-EE97-4a90-B076-33F57BF4EAA7}"
 
@@ -203,30 +201,6 @@ class Snapshot:
 
 
 if __name__ == '__main__':
-    args = sys.argv[1:]
-    if '--install' in args:
-        shortcut_file = os.path.expanduser(
-            '~\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\RestoreWindowPos.lnk')
-        if os.path.isfile(shortcut_file):
-            os.remove(shortcut_file)
-
-        shortcut = pythoncom.CoCreateInstance(
-            shell.CLSID_ShellLink,
-            None,
-            pythoncom.CLSCTX_INPROC_SERVER,
-            shell.IID_IShellLink
-        )
-        shortcut.SetPath(sys.executable.replace('python.exe', 'pythonw.exe'))
-        shortcut.SetArguments(__file__)
-        shortcut.SetWorkingDirectory(local_path('..'))
-        shortcut.SetIconLocation(local_path(
-            'assets/icon32.ico', asset=True), 0)
-
-        persist_file = shortcut.QueryInterface(pythoncom.IID_IPersistFile)
-        persist_file.Save(shortcut_file, 0)
-
-        sys.exit(0)
-
     EXIT = False
 
     def notify(*_):
