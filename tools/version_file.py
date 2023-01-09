9bdf95ce7b66e3caf0668cd114af877ee2d2701d
Crozzers <captaincrozzers@gmail.com>
2023-01-09 18:38:37 +0000

    Use version number from `src` in `tools\version_file`

--- tools/version_file.py
+++ tools/version_file.py
@@ -1,13 +1,17 @@
 import os
+import sys
 
 import pyinstaller_versionfile
+
+sys.path.insert(0, 'src')
+from _version import __version__  # noqa: E402
 
 if not os.path.isdir('build'):
     os.mkdir('build')
 
 pyinstaller_versionfile.create_versionfile(
     output_file="build/versionfile.txt",
-    version="0.4.0.0",
+    version=f'{__version__}.0',
     file_description="RestoreWindowPos",
     legal_copyright="© Crozzers (github.com/Crozzers) 2023",
     product_name="RestoreWindowPos"
