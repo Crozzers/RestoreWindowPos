1f1abe5eee5b1dcbe55596863f9784dbde0b53a6
Crozzers <captaincrozzers@gmail.com>
2023-01-09 18:40:18 +0000

    Auto increment build numbers
    The version file generator will now read the previous version file and determine what
    the previous build's MAJOR.MINOR.MICRO.BUILD version is. It will then compare this to
    the current MAJOR.MINOR.MICRO version of the source code.
    If the two are the same, the build number will be incremented.
    Otherwise, it gets reset to 0

--- tools/version_file.py
+++ tools/version_file.py
@@ -1,18 +1,32 @@
 import os
+import re
 import sys
 
 import pyinstaller_versionfile
+from packaging.version import Version
 
 sys.path.insert(0, 'src')
 from _version import __version__  # noqa: E402
 
-if not os.path.isdir('build'):
+OUTPUT_FILE = 'build/versionfile.txt'
+
+build = 0
+if os.path.isdir('build') and os.path.isfile(OUTPUT_FILE):
+    with open(OUTPUT_FILE) as f:
+        ver_str = re.search(r'filevers=\((\d+,\d+,\d+),(\d+)\)', f.read(), re.M)
+
+    last_version = Version(ver_str.group(1).replace(',', '.'))
+    last_build = int(ver_str.group(2))
+
+    if Version(__version__) == last_version:
+        build = last_build + 1
+else:
     os.mkdir('build')
 
 pyinstaller_versionfile.create_versionfile(
-    output_file="build/versionfile.txt",
-    version=f'{__version__}.0',
-    file_description="RestoreWindowPos",
-    legal_copyright="© Crozzers (github.com/Crozzers) 2023",
-    product_name="RestoreWindowPos"
+    output_file=OUTPUT_FILE,
+    version=f'{__version__}.{build}',
+    file_description='RestoreWindowPos',
+    legal_copyright='© Crozzers (github.com/Crozzers) 2023',
+    product_name='RestoreWindowPos'
 )
