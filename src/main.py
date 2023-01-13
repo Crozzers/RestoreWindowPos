f8fb9c77e062f3eb4550922e95c68ffa3876557a
Crozzers <captaincrozzers@gmail.com>
2023-01-13 23:00:45 +0000

    Fix restore "Most recent" button not doing anything

--- src/main.py
+++ src/main.py
@@ -75,7 +75,7 @@ if __name__ == '__main__':
     menu_options = [
         ['Capture Now', None, lambda s: snap.update()],
         ['Pause Snapshots', None, pause_snapshots],
-        ['Restore Snapshot', None, [['Most recent', None, lambda s: snap.restore]]],
+        ['Restore Snapshot', None, [['Most recent', None, lambda s: snap.restore()]]],
         [
             "Snapshot frequency", None, submenu_from_settings(
                 SETTINGS, 'snapshot_freq', 30, 'second', [2, 5, 30, 60, 300])
