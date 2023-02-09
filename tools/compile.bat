724dddb02e480b1c1c44cd46b1a4f3d3c17b784a
Crozzers <captaincrozzers@gmail.com>
2023-02-09 20:29:34 +0000

    Include license with bundled EXE

--- tools/compile.bat
+++ tools/compile.bat
@@ -1,3 +1,3 @@
 python tools/version_file.py
-pyinstaller -F -w --version-file "build/versionfile.txt" --collect-all infi.systray --add-data "./assets/icon32.ico;./assets" -i "assets/icon256.ico" -n RestoreWindowPos "src/main.py"
+pyinstaller -F -w --version-file "build/versionfile.txt" --collect-all infi.systray --add-data "./assets/icon32.ico;./assets" --add-data "./LICENSE;./" -i "assets/icon256.ico" -n RestoreWindowPos "src/main.py"
 makensis "tools/installer.nsi"
