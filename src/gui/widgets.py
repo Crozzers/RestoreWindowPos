941da669bc2179922640518d0834385be74c52d5
Crozzers <captaincrozzers@gmail.com>
2023-02-19 20:41:07 +0000

    Add missing widgets file from previous commit

---
+++ src/gui/widgets.py
@@ -0,0 +1,42 @@
+import wx
+
+from common import local_path
+
+
+class Frame(wx.Frame):
+    def __init__(self, parent=None, title=None, **kwargs):
+        if title is None:
+            title = 'RestoreWindowPos'
+        else:
+            title = f'{title} - RestoreWindowPos'
+        wx.Frame.__init__(self, parent=parent, id=wx.ID_ANY,
+                          title=title, **kwargs)
+        self.SetIcon(wx.Icon(local_path('assets/icon32.ico', asset=True)))
+
+    def GetIdealSize(self):
+        x = 480
+        y = 360
+        for child in self.GetChildren():
+            if hasattr(child, 'GetBestVirtualSize'):
+                vsize = child.GetBestVirtualSize()
+                x = max(x, vsize.x)
+                y = max(y, vsize.y)
+        return wx.Size(x, y)
+
+    def SetIdealSize(self):
+        self.SetSize(self.GetIdealSize())
+
+
+class ListCtrl(wx.ListCtrl):
+    def __init__(self, parent, *args, **kwargs):
+        kwargs.setdefault('style', wx.LC_REPORT)
+        wx.ListCtrl.__init__(self, parent, *args, **kwargs)
+
+    def GetAllSelected(self):
+        pos = self.GetFirstSelected()
+        if pos == -1:
+            return
+        yield pos
+        while (item := self.GetNextSelected(pos)) != -1:
+            yield item
+            pos = item
