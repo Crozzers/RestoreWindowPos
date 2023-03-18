8e2f1c2ed99c69e86b5f4aac53af93ea39701390
Crozzers <captaincrozzers@gmail.com>
2023-03-18 11:57:49 +0000

    Add "support" for semver suffixes in build process
    Suffixes like "-dev1" are just ignored.

--- tools/version_file.py
+++ tools/version_file.py
@@ -7,6 +7,7 @@ from packaging.version import Version
 
 sys.path.insert(0, 'src')
 from _version import __version__  # noqa: E402
+__basic_version__ = __version__.split('-')[0]
 
 OUTPUT_FILE = 'build/versionfile.txt'
 
@@ -19,14 +20,14 @@ if os.path.isdir('build'):
         last_version = Version(ver_str.group(1).replace(',', '.'))
         last_build = int(ver_str.group(2))
 
-        if Version(__version__) == last_version:
+        if Version(__basic_version__) == last_version:
             build = last_build + 1
 else:
     os.mkdir('build')
 
 pyinstaller_versionfile.create_versionfile(
     output_file=OUTPUT_FILE,
-    version=f'{__version__}.{build}',
+    version=f'{__basic_version__}.{build}',
     file_description='RestoreWindowPos',
     legal_copyright='© Crozzers (github.com/Crozzers) 2023',
     product_name='RestoreWindowPos'
